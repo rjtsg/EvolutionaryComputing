48ca6000d2101cc2330c90254e1ca91f12110e5f
rjtsg <rjtsg@users.noreply.github.com>
2019-06-19 22:00:13 +0200

    Optimize functions works, update runfile to run optimization

--- RunFile.py
+++ RunFile.py
@@ -7,6 +7,7 @@ Created on Tue Jun 18 15:40:59 2019
 # %% Import stuff
 import FunctionFile as FF
 import EvaluationFile as EF
+import SearchFile as SF
 
 # %%Write here 'global' parameters needed
 N = 100 #population size
@@ -17,16 +18,21 @@ Exec = 10 #number of executions of the algorithm
 Fraction = 0.5 #Fraction of the parents dies
 NM = 5 #Amount of members that undergo a mutation
 Scaling = 1.25 #Border scaling parameter
+tol = 0.5  #the tolerance limit
+limit = 10000 #the upper limit of generations
+## %% Call here the functions
+#members = FF.InitialPopulation(N,a,b)
+#frosen = FF.PopEval(members)
+#Parent1,Parent2 = FF.ParentSelection(members,frosen)
+#NewMembers = FF.ReproductionLife(Parent1,Parent2,Fraction)
+#MutatedMembers = FF.MutationXX(NewMembers,NM,a,b)
+#members1,OriginalMembers = FF.RunEvolution(N,a,b,gen,Fraction)
+#members2,OriginalMembers2 = FF.BaseEvolution(members,gen,Fraction,NM,Scaling)
+#
+## %% Call here evaluation files
+#output1 = EF.QuickComparison(gen,members,Fraction,NM,Scaling,a,b)
+#output2 = EF.UQ(Exec,gen,N,a,b,Fraction,NM,Scaling)
 
-# %% Call here the functions
-members = FF.InitialPopulation(N,a,b)
-frosen = FF.PopEval(members)
-Parent1,Parent2 = FF.ParentSelection(members,frosen)
-NewMembers = FF.ReproductionLife(Parent1,Parent2,Fraction)
-MutatedMembers = FF.MutationXX(NewMembers,NM,a,b)
-members1,OriginalMembers = FF.RunEvolution(N,a,b,gen,Fraction)
-members2,OriginalMembers2 = FF.BaseEvolution(members,gen,Fraction,NM,Scaling)
-
-# %% Call here evaluation files
-output1 = EF.QuickComparison(gen,members,Fraction,NM,Scaling,a,b)
-output2 = EF.UQ(Exec,gen,N,a,b,Fraction,NM,Scaling)
+# %% Call here the optimization function
+Minimum = SF.Minimize(tol,limit,N,a,b,Fraction,NM,Scaling)
+print(Minimum)
--- SearchFile.py
+++ SearchFile.py
@@ -7,28 +7,32 @@ Created on Wed Jun 19 19:27:05 2019
 
 import FunctionFile as FF
 import numpy as np
-tol = 1
-i = 0
 
-# %%Write here 'global' parameters needed
-N = 100 #population size
-a = -1 #lower starting bound
-b = 3  #upper starting bound
-gen = 3 #Specify how many generations you want to have
-Exec = 10 #number of executions of the algorithm
-Fraction = 0.5 #Fraction of the parents dies
-NM = 5 #Amount of members that undergo a mutation
-Scaling = 1.25 #Border scaling parameter
-gen = 0 #keep track of the number of generations that passed
-members = FF.InitialPopulation(N,a,b)
-frosen = FF.PopEval(members)
-frosenBest = np.amin(frosen)
-while frosenBest > tol:
-    PreviousBest = frosenBest
-    Parent1,Parent2 = FF.ParentSelection(members,frosen)
-    members = FF.ReproductionLife(Parent1,Parent2,Fraction)
-    members = FF.MutationBorderScale(members,NM,Scaling)
+
+## %%Write here 'global' parameters needed
+#N = 100 #population size
+#a = -1 #lower starting bound
+#b = 3  #upper starting bound
+#gen = 3 #Specify how many generations you want to have
+#Exec = 10 #number of executions of the algorithm
+#Fraction = 0.5 #Fraction of the parents dies
+#NM = 5 #Amount of members that undergo a mutation
+#Scaling = 1.25 #Border scaling parameter
+
+def Minimize(tol,limit,N,a,b,Fraction,NM,Scaling):
+    gen = 0 #keep track of the number of generations that passed
+    members = FF.InitialPopulation(N,a,b)
     frosen = FF.PopEval(members)
     frosenBest = np.amin(frosen)
-    gen +=1
-    print('Generation: %.f, f(x) = %.3f, df(x) = %.3f' % (gen,frosenBest,frosenBest-PreviousBest))
+    while frosenBest > tol and gen < limit:
+        PreviousBest = frosenBest
+        Parent1,Parent2 = FF.ParentSelection(members,frosen)
+        members = FF.ReproductionLife(Parent1,Parent2,Fraction)
+        members = FF.MutationBorderScale(members,NM,Scaling)
+        frosen = FF.PopEval(members)
+        frosenBest = np.amin(frosen)
+        Index = np.where(frosen == np.amin(frosen))
+        minimum = members[Index[0][0]]
+        gen +=1
+        print('Generation: %.f, f(x) = %.3f, df(x) = %.3f' % (gen,frosenBest,frosenBest-PreviousBest))
+    return minimum
